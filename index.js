feb337bd27b7f60d915e69e3aeebcfa099a1b322
Filit0o0 <120840092+Filit0o0@users.noreply.github.com>
2022-12-17 16:57:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sabia que ibas a decir que si, ya que no encontraras nadie como yo ❤ ')
+})
+
+noBtn.addEventListener('click',function () {
+    alert('Esta opcion era muy poco probable, pero yo siempre te amare ya que eres una mujer increible, romantica, amable, unica y quiero aprovechar el tiempo contigo')
+})
